db9a98d1dff629b85e563afd73a92dfa2096f822
miseryprevails <77559209+miseryprevails@users.noreply.github.com>
2025-06-23 18:04:35 -0400

    Initial commit

---
+++ Starter Summer project.py
@@ -0,0 +1,8 @@
+print("Welcome to your new interesting world...")
+choice = input("Would you like to go Left or Right?")
+
+if choice.lower() == "left":
+    print("you have fell in a deep hole and can't get out..GAME OVER!!")
+else: 
+    print("you have found a golden...stick..good for you..YOU WON!!")
+
